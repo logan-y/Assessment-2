161a6f0d86e21225fa4ce0465d1de4ecb0b753bb
Logan Young <logany@att.net>
2021-10-24 20:30:22 -0500

    finished menu file

--- menu.js
+++ menu.js
@@ -6,8 +6,6 @@
     deals with food objects, arrays of objects
     and filtering those arrays. 
 */
-
-
 //////////////////PROBLEM 1////////////////////
 /*  
     Create an object called `pizza` that has 6
@@ -30,10 +28,14 @@
     'kids'
 */
 
-//CODE HERE
-
-
-
+const pizza = {
+    name: 'pizza',
+    price: 13.99,
+    category: 'flat food entree',
+    popularity: 9,
+    rating: 5,
+    tags: ['fast food', 'pepperoni', 'crust', 'cheese']
+}
 //////////////////PROBLEM 2////////////////////
 /* 
     Let's print a few values from our pizza object.
@@ -41,40 +43,30 @@
     First, log the popularity of pizza. 
     Use dot notation to access the value.
 */
-
-//CODE HERE
-
-
+console.log(pizza.popularity);
 /*
     Second, log the second tag in your pizza's
     tags array.
     Use a combination of dots and brackets to
     get the value.
 */
-
-//CODE HERE
-
-
+console.log(pizza.tags[1]);
 /*
     Third, destructure the price off of the
     pizza object.
     
     Print the value of your new price variable.
 */
-
-//CODE HERE
-
-
+let {price: pizzaPrice} = pizza;
+console.log(pizzaPrice);
 /*
     Fourth, and last, destructure the category
     property.
 
     Print the value of your category variable. 
 */
-
-//CODE HERE
-
-
+let {category: cat} = pizza;
+console.log(cat);
 //////////////////PROBLEM 3////////////////////
 /* 
     Create an array with about 5 objects in it.
@@ -86,11 +78,48 @@
     tags. That way, you'll be able to use this
     data in some functions that you'll write.
 */
-
-//CODE HERE
-
-
-
+let foodArr = [
+    { 
+    name: 'watermelon',
+    price: 11.99,
+    category: 'fresh appetizer',
+    popularity: 80,
+    rating: 4,
+    tags: ['seeds', 'fresh', 'juicy', 'messy']
+    },
+    {
+    name: 'boudin',
+    price: 12.99,
+    category: 'cajun food pyramid third tier',
+    popularity: 70,
+    rating: 3.8,
+    tags: ['meats', 'spices', 'rices', 'casing']
+    },
+    {
+    name: 'creme brulee',
+    price: 14.99,
+    category: 'fancy dessert!',
+    popularity: 60,
+    rating: 3,
+    tags: ['sugar', 'cream', 'dainty']
+    },
+    {
+    name: 'cauliflower buffalo wings',
+    price: 15.99,
+    category: 'hip entree',
+    popularity: 50,
+    rating: 2.5,
+    tags: ['vegan', 'hot sauce', 'cauliflower', 'panko']
+    },
+    {
+    name: 'brocci casseroli',
+    price: 16.99,
+    category: 'potluck entree',
+    popularity: 40,
+    rating: 2,
+    tags: ['steamy food', 'broccoli', 'cheese', 'cream']
+    }
+]
 //////////////////PROBLEM 4////////////////////
 /* 
     Let's filter the food objects according
@@ -103,12 +132,20 @@
     your food objects has.
 */
 
-//CODE HERE
+// let creamFilter = (foodArrElem) => {
+//     let taggedArr = [];
+//     for( let i = 0; i < foodArrElem.length; i++)
+//     if(foodArrElem[i].tags.includes('cream')){
+//     taggedArr.push(foodArrElem[i])
+//     }
+// return taggedArr;
+// }
+//console.log(creamFilter(foodArr)); my initial attempt at filtering function
 
-// const filteredFood = foodArr.filter(/* CALLBACK HERE */)
 
 
-
+const filteredFood = foodArr.filter(elem => elem.tags.includes('cream'));
+//console.log(filteredFood);
 //////////////////PROBLEM 5////////////////////
 /* 
     Now let's write a function that's a little
@@ -148,14 +185,22 @@
     Return the filtered array from the entire function
 */
 
-//CODE HERE
+let filterByProperty = (property, number, type) => {
+    filteredArr = [];
 
-
+    if(type == 'above'){
+        filteredArr = foodArr.filter(elem => elem[property] > number)
+    } else if (type == 'below'){
+        filteredArr = foodArr.filter(elem => elem[property] < number)
+    } else {
+        console.log('This is unacceptable');
+    }
+    return filteredArr;
+}
+console.log(filterByProperty('rating', 3, 'below'));
 /*
     Invoke the `filterByProperty` function passing
     in a value for each paramter.
 
     You'll have to console.log to see the filtered array
 */
-
-//CODE HERE
